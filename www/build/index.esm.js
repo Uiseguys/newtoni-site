7d3223a86b039b39865074b7f0c42e8f33a9d6f9
Cris Shaki <cmshaki@gmail.com>
2020-11-24 10:52:27 +0300

    git removed cached www folder

--- www/build/index.esm.js
+++
@@ -1,8 +0,0 @@
-import './index-660b38e0.js';
-import { A as ActiveRouter } from './active-router-251d9f34.js';
-import './match-path-760e1797.js';
-import './location-utils-fea12957.js';
-
-function injectHistory(Component) {
-    ActiveRouter.injectProps(Component, ['history', 'location']);
-}
